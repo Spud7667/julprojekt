7f895579a91a5593def593e022d1666911d24bd6
Spud7667 <158800318+Spud7667@users.noreply.github.com>
2026-01-04 15:47:29 +0100

    lol2

--- js/script.js
+++ js/script.js
@@ -8,10 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (clickCount === 1) {
       // Första klicket: julrim
       message.innerHTML = `
-        Ej något som samlar damm på en hylla,<br>
-        istället en kväll som sinnet ska fylla.<br>
-        Mat, dryck och lyx i perfekt balans,<br>
-        en middag på Moon Thai med härlig elegans.
+        Snö!
       `;
       message.style.color = "green";
       message.style.fontSize = "1.0rem";
@@ -19,12 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     } else if (clickCount === 2) {
       // Andra klicket: inbjudningskort
       message.innerHTML = `
-        <div class="invitation">
-          <h4>Inbjudningskort</h4>
-          <p>Moon Thai Kitchen</p>
-          <p>Den 30/12 klockan 18:00</p>
-          <p>En kväll med mat, dryck och härlig atmosfär</p>
-        </div>
+        Kyssar!
       `;
       message.style.color = "";       // låt texten följa invitation-klassens färg
       message.style.fontSize = "";    // återställ font-size
